14d15901dd565b6823510d21eeb1ec60ae760d6f
Mick Watkins <mick_watkins94@hotmail.com>
2017-06-26 14:53:06 +0000

    Added creation and deletion of users with testing in test file.

--- test/test_functions.php
+++ test/test_functions.php
@@ -1,6 +1,17 @@
 <?php
 include '../php/mcLogin.php';
+echo "Testing checkLogin" . PHP_EOL;
 echo checkLogin('john','doe') . PHP_EOL;
 echo checkLogin('john') . PHP_EOL;
 echo checkLogin('mick') . PHP_EOL;
+
+echo "Testing createUser" . PHP_EOL;
+echo createUser('mick','watkins') . PHP_EOL;
+echo createUser('bridget') . PHP_EOL;
+echo createUser('john','doe') . PHP_EOL;
+
+echo "Testing createUser" . PHP_EOL;
+echo deleteUser('bridget') . PHP_EOL;
+echo deleteUser('mick') . PHP_EOL;
+echo deleteUser('doesnt_exist') . PHP_EOL;
 ?>
